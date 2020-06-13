c5982c78c424656033346f210a14bdd1e9a714fb
Barros <giu.barreca@gmail.com>
2020-06-13 17:00:45 +0200

    Initial commit, setting gradle and splash screen

---
+++ app/src/main/java/com/barros/architecturecompare/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.barros.architecturecompare
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
